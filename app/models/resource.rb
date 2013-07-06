fe4e4f0031062c061277c12291798bd0a3138560
Xiao Jin <freelog.jin@gmail.com>
2013-07-06 16:25:58 -0700

    created resource model and made resource_id hash key as default identifier

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,20 @@
+require 'digest/sha2'
+
 class Resource < ActiveRecord::Base
+
+  validates_presence_of :resource_id
+  validates_presence_of :content
+  validates_presence_of :sharing_policy
+  validates_presence_of :resource_type
+  validates_uniqueness_of :resource_id
+
+  def to_param
+    self.resource_id
+  end
+
+  before_validation(on: :create) do
+    sha256 = Digest::SHA256.new
+    self.resource_id = sha256.hexdigest(self.content) if not attribute_present?("resource_id")
+  end
+
 end
